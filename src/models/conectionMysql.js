0ecde4f9d38b431fa1ca867bbd7ac4f1cbe4399a
Kawan <69736349+K4w4n@users.noreply.github.com>
2021-06-23 16:43:52 -0300

    Fazendo a limpa

--- src/models/conectionMysql.js
+++
@@ -1,9 +0,0 @@
-const config = require('dotenv').config();
-const mysql = require('mysql2');
-exports.connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    database: process.env.database,
-    password: process.env.password
-});
-exports.mysql = mysql;
