cc3bedac3453ce23d6ee9e0a782a4337d9bb8031
Kawan Araújo <kawanaraujocontato@gmail.com>
2021-06-23 17:39:54 -0300

    Restaurando a conexão mysql com algumas informações para o bd de testes

--- src/models/conectionMysql.js
+++ src/models/conectionMysql.js
@@ -1,9 +1,9 @@
 const config = require('dotenv').config();
 const mysql = require('mysql2');
 exports.connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    database: process.env.database,
-    password: process.env.password
+    host: process.env.host || 'localhost',
+    user: process.env.user || 'app',
+    database: process.env.database || 'db_ceos',
+    password: process.env.password || '1RP9n3yCi&Y8jpdD2PLf@g@%^LKu5tVcQSL&4ASeSOpt%4UoHe'
 });
 exports.mysql = mysql;
