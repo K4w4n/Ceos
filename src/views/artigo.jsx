8e0d7be60befc3d25c1987128a25074e31f0b72c
Kawan Araújo <kawanaraujocontato@gmail.com>
2021-08-06 21:45:10 -0300

    Criando design basico para testar pequenas funcionalidades

--- src/views/artigo.jsx
+++ src/views/artigo.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 module.exports = (data) => {
-    const paragrafos = data.conteudo.split("\n");
+    const paragrafos = data.conteudo ? data.conteudo.split("\n") : ['Sem conteudo'];
     const paragrafosHTML = paragrafos.map(p => <p align="justify">{p}</p>);
 
     return <html lang="pt-br">
         <head>
             <meta charset="utf-8" />
-            <title>{`${data.titulo} - ${data.nomeAutor} ${data.sobrenomeAutor}`}</title>
+            <title>{`${data.titulo || 'Sem titulo'} - ${data.nomeAutor} ${data.sobrenomeAutor}`}</title>
             <link rel="stylesheet" href="/files/css/estiloProvisorio.css" />
         </head>
         <body>
             <h1>
-                {data.titulo}
+                {data.titulo || 'Sem titulo'}
             </h1>
             {paragrafosHTML}
             <label>
