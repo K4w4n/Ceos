c65829a4e78e6340897c28daea2c9df094b8834a
Kawan Araújo <kawanaraujocontato@gmail.com>
2021-08-11 09:07:11 -0300

    gerando gambiarra temporaria para teste

--- src/models/conectionMysql.js
+++ src/models/conectionMysql.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
 const config = dotenv.config();
 import mysql2 from 'mysql2';
-export default mysql2.createConnection({
+/* export default mysql2.createConnection({
     host: process.env.host || 'localhost',
     user: process.env.user || 'app',
     database: process.env.database || 'db_ceos',
     password: process.env.password || '1RP9n3yCi&Y8jpdD2PLf@g@%^LKu5tVcQSL&4ASeSOpt%4UoHe'
-});
+}); */
+export default {};
