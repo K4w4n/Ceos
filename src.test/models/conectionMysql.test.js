decbf37e6bdc339da1a6aea57a2002c38ccf3c36
Kawan Araújo <kawanaraujocontato@gmail.com>
2021-10-15 23:52:28 -0300

    Criando teste para Operation

--- src.test/models/conectionMysql.test.js
+++ src.test/models/conectionMysql.test.js
@@ -1,110 +1,59 @@
-import { Mysql, connection } from "../../src/models/conectionMysql";
+import { Operation, connection } from "../../src/models/conectionMysql";
 
-const bancoDeDados = new Mysql();
-
-describe('Testando get no Mysql', () => {
-    it('Query para pegar todos os produtos com quantidade menor igual a 5', () => {
-        return bancoDeDados.get({
-            tabela: 'tb_testes',
-            configBusca: [{
-                coluna: 'item_quantidade',
-                operador: '<=',
-                valor: 5
-            }],
-            quantidade: 3
-        }).then(dados => {
-            expect(dados).toEqual([
-                { 'item_id': 1, 'item_name': 'colher', 'item_quantidade': 5 },
-                { 'item_id': 3, 'item_name': 'garfo', 'item_quantidade': 3 },
-                { 'item_id': 5, 'item_name': 'casada', 'item_quantidade': 1 }
-            ]);
-            return dados;
-        });
-    });
-
-    it('query para pegar a quantidade de sapatos', () => {
-        return bancoDeDados.get({
-            tabela: 'tb_testes',
-            buscar: { 'item_name': 'sapato' },
-            colunas: ['item_quantidade'],
-        }).then(dados => {
-            expect(dados).toEqual([{ 'item_quantidade': 88 }]);
-        });
-    });
-
-    it('query para pegar os 5 itens com id menor que 8 e que sejam diferentes de 5', () => {
-        return bancoDeDados.get({
-            tabela: 'tb_testes',
-            configBusca: [
-                {
-                    coluna: 'item_id',
-                    operador: '<',
-                    valor: 8,
-                },
-                {
-                    coluna: 'item_id',
-                    operador: '!=',
-                    valor: 5,
-                },
-            ],
-            quantidade: 5,
-        }).then(dados => {
-            expect(dados).toEqual([
-                { 'item_id': 1, 'item_name': 'colher', 'item_quantidade': 5 },
-                { 'item_id': 3, 'item_name': 'garfo', 'item_quantidade': 3 },
-                { 'item_id': 4, 'item_name': 'arma', 'item_quantidade': 15 },
-                { 'item_id': 6, 'item_name': 'sapato', 'item_quantidade': 88 },
-                { 'item_id': 7, 'item_name': 'papeis', 'item_quantidade': 8 },
-            ]);
-        });
-    });
-    it('query para pegar produtos com quantidade igual a 7 e id menor que 10', () => {
-        return bancoDeDados.get({
-            tabela: 'tb_testes',
-            configBusca: [
-                { coluna: 'item_quantidade', operador: '=', valor: 7 },
-                { coluna: 'item_id', operador: '<', valor: 10 },
-            ]
-        }).then(dados => {
-            expect(dados).toEqual([
-                { 'item_id': 8, 'item_name': 'casaco', 'item_quantidade': 7 },
-                { 'item_id': 9, 'item_name': 'maçam', 'item_quantidade': 7 },
-            ]);
-        });
+describe('async jest com mysql2', () => {
+    it('testando jest', async () => {
+        expect(0).toBe(0);
+        await connection.end();
     });
 });
 
-describe('Testando set do mysql', () => {
-    it('inserindo itens robos', async () => {
-        const dadosA = await bancoDeDados.set({
-            tabela: 'tb_testes',
-            colunas: ['item_name', 'item_quantidade'],
-            valores: [['item Bot ' + Math.round(Math.random() * 100), Math.round(Math.random() * 10)]]
-        });
-        const dadosB = await bancoDeDados.get({
-            tabela: 'tb_testes',
-            ordem: {
-                ordenadores: ['item_id'],
-                crescente: false
-            },
-            quantidade: 1
-        });
-        expect(dadosA[0].insertId).toBe(dadosB[0]['item_id']);
+describe('Operation', () => {
+    describe('equal', () => {
+        it('user_email = ?', () => expect(new Operation().column('user_email').equal.value('kawan@gmail.com').toString()).toBe('user_email = ?'));
+        it('? = user_email', () => expect(new Operation().value('kawan@gmail.com').equal.column('user_email').toString()).toBe('? = user_email'));
+        it('? = ?', () => expect(new Operation().value('kawan@gmail.com').equal.value('kawan@gmail.com').toString()).toBe('? = ?'));
+        it('user_email = user_email', () => expect(new Operation().column('user_email').equal.column('user_email').toString()).toBe('user_email = user_email'));
     });
-
-    it('testando Catch', () => {
-        return bancoDeDados.set({
-            tabela: 'tb_testes',
-            colunas: ['item_name', 'item_quantidade'],
-            valores: [
-                ['algodão', 5],
-                ['energetico', 3],
-                ['sandalia', 12],
-            ]
-        }).then(() => {
-            expect(true).toBe(false);
-        }).catch(erro => {
-            expect(true).toBe(true);
-        });
+    describe('lesser', () => {
+        it('user_idade < ?', () => expect(new Operation().column('user_idade').lesser.value(18).toString()).toBe('user_idade < ?'));
+        it('? < user_idade', () => expect(new Operation().value(18).lesser.column('user_idade').toString()).toBe('? < user_idade'));
+        it('? < ?', () => expect(new Operation().value(5).lesser.value(18).toString()).toBe('? < ?'));
+        it('user_idade < user_maioridade', () => expect(new Operation().column('user_idade').lesser.column('user_maioridade').toString()).toBe('user_idade < user_maioridade'));
+    });
+    describe('lesser equal', () => {
+        it('user_idade <= ?', () => expect(new Operation().column('user_idade').lesserEqual.value(18).toString()).toBe('user_idade <= ?'));
+        it('? <= user_idade', () => expect(new Operation().value(18).lesserEqual.column('user_idade').toString()).toBe('? <= user_idade'));
+        it('? <= ?', () => expect(new Operation().value(5).lesserEqual.value(18).toString()).toBe('? <= ?'));
+        it('user_idade <= user_maioridade', () => expect(new Operation().column('user_idade').lesserEqual.column('user_maioridade').toString()).toBe('user_idade <= user_maioridade'));
+    });
+    describe('greater', () => {
+        it('user_idade > ?', () => expect(new Operation().column('user_idade').greater.value(18).toString()).toBe('user_idade > ?'));
+        it('? > user_idade', () => expect(new Operation().value(18).greater.column('user_idade').toString()).toBe('? > user_idade'));
+        it('? > ?', () => expect(new Operation().value(18).greater.value(5).toString()).toBe('? > ?'));
+        it('user_idade > user_maioridade', () => expect(new Operation().column('user_idade').greater.column('user_maioridade').toString()).toBe('user_idade > user_maioridade'));
+    });
+    describe('greater equal', () => {
+        it('user_idade >= ?', () => expect(new Operation().column('user_idade').greaterEqual.value(18).toString()).toBe('user_idade >= ?'));
+        it('? >= user_idade', () => expect(new Operation().value(18).greaterEqual.column('user_idade').toString()).toBe('? >= user_idade'));
+        it('? >= ?', () => expect(new Operation().value(5).greaterEqual.value(18).toString()).toBe('? >= ?'));
+        it('user_idade >= user_maioridade', () => expect(new Operation().column('user_idade').greaterEqual.column('user_maioridade').toString()).toBe('user_idade >= user_maioridade'));
+    });
+    describe('different', () => {
+        it('user_idade != ?', () => expect(new Operation().column('user_idade').different.value(18).toString()).toBe('user_idade != ?'));
+        it('? != user_idade', () => expect(new Operation().value(18).different.column('user_idade').toString()).toBe('? != user_idade'));
+        it('? != ?', () => expect(new Operation().value(5).different.value(18).toString()).toBe('? != ?'));
+        it('user_idade != user_maioridade', () => expect(new Operation().column('user_idade').different.column('user_maioridade').toString()).toBe('user_idade != user_maioridade'));
+    });
+    describe('and', () => {
+        it('user_idade > ? AND user_name != ?', () => expect(new Operation().column('user_idade').greater.value(18).and.column('user_name').different.value('kawan').toString()).toBe('user_idade > ? AND user_name != ?'));
+        it('user_idade >= ? AND user_name != ?', () => expect(new Operation().column('user_idade').greaterEqual.value(18).and.column('user_name').different.value('kawan').toString()).toBe('user_idade >= ? AND user_name != ?'));
+        it('user_idade < ? AND user_name = ?', () => expect(new Operation().column('user_idade').lesser.value(18).and.column('user_name').equal.value('kawan').toString()).toBe('user_idade < ? AND user_name = ?'));
+        it('user_idade <= ? AND user_name = ?', () => expect(new Operation().column('user_idade').lesserEqual.value(18).and.column('user_name').equal.value('kawan').toString()).toBe('user_idade <= ? AND user_name = ?'));
+    });
+    describe('or', () => {
+        it('user_idade > ? OR user_name != ?', () => expect(new Operation().column('user_idade').greater.value(18).or.column('user_name').different.value('kawan').toString()).toBe('user_idade > ? OR user_name != ?'));
+        it('user_idade >= ? OR user_name != ?', () => expect(new Operation().column('user_idade').greaterEqual.value(18).or.column('user_name').different.value('kawan').toString()).toBe('user_idade >= ? OR user_name != ?'));
+        it('user_idade < ? OR user_name = ?', () => expect(new Operation().column('user_idade').lesser.value(18).or.column('user_name').equal.value('kawan').toString()).toBe('user_idade < ? OR user_name = ?'));
+        it('user_idade <= ? OR user_name = ?', () => expect(new Operation().column('user_idade').lesserEqual.value(18).or.column('user_name').equal.value('kawan').toString()).toBe('user_idade <= ? OR user_name = ?'));
     });
 });
